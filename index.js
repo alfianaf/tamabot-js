bb4dbf156848529f0eee54b6e5356aaea47fcab5
alfianaf <alfianaf123@gmail.com>
2020-10-07 19:53:15 +0700

    a

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ client.on('message', message => {
 
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
-            message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
             
-            return message.author.bot.delete({ timeout: 2000 });
+            // message.author.bot.delete({ timeout: 2000 });
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`).then(msg=>{msg.delete({timeout:2000})});
         }
     }
     
