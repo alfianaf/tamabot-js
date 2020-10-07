9fa763864fa754f9332607643a48ae91b7424fce
alfianaf <alfianaf123@gmail.com>
2020-10-07 17:27:16 +0700

    a

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,17 +6,8 @@ module.exports = {
 
 
         if (!message.mentions.users.size) {
-            const avatarList = message.mentions.users.map(user => {
-                return `${user.displayAvatarURL({ format: "png", dynamic: true })}`;
-            });
-            
-            const exampleEmbed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle(`${taggedUser.username}'s Avatar`)
-                .setImage(`${avatarList[0]}`)
-            
-            channel.send(exampleEmbed);
-            // return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
         }
         // const user = message.mentions.users.displayAvatarURL({ format: "png", dynamic: true });
         const avatarList = message.mentions.users.map(user => {
@@ -28,6 +19,6 @@ module.exports = {
             .setTitle(`${taggedUser.username}'s Avatar`)
             .setImage(`${avatarList[0]}`)
         
-        channel.send(exampleEmbed);
+        message.channel.send(exampleEmbed);
 	},
 };
