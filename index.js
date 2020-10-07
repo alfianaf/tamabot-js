7c1db443bfed865bb070d3df032616ee91982031
alfianaf <alfianaf123@gmail.com>
2020-10-07 13:36:17 +0700

    first

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    console.log(message.content);
+
+    // if (message.content === `${prefix}ping`) {
+    //     message.channel.send('Pong.');
+    // } else if (message.content === `${prefix}beep`) {
+    //     message.channel.send('Boop.');
+    // }
+    if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send('Pong.');
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('Boop.');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+    // using the new `command` variable, this makes it easier to manage!
+    // you can switch your other commands to this format as well
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+    else if (command === 'kick') {
+
+        // grab the "first" mentioned user from the message
+        // this will return a `User` object, just like `message.author`
+        const taggedUser = message.mentions.users.first();
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+
+    }
+    else if (command === 'avatar') {
+        const taggedUser = message.mentions.users.first();
+
+
+        if (!message.mentions.users.size) {
+
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        }
+        // const user = message.mentions.users.displayAvatarURL({ format: "png", dynamic: true });
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.displayAvatarURL({ format: "png", dynamic: true })}`;
+        });
+
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${taggedUser.username}'s Avatar`)
+            .setImage(`${avatarList[0]}`)
+
+        message.channel.send(exampleEmbed);
+
+    }
+
+});
+
+client.login(process.env.BOT_TOKEN);
