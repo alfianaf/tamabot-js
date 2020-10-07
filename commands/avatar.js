e4181753f3aac85a738bba5aaf5708afe76a76ea
alfianaf <alfianaf123@gmail.com>
2020-10-07 18:04:47 +0700

    a

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,25 +1,33 @@
 const Discord = require('discord.js');
 module.exports = {
-	name: 'avatar',
-	execute(message, args) {
+    name: 'avatar',
+    execute(message, args) {
 
         const taggedUser = message.mentions.users.first();
 
 
         if (!message.mentions.users.size) {
-        
-            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+
+            // return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+            const user = message.author.displayAvatarURL({ format: "png", dynamic: true });
+
+            const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`${message.author.username}'s Avatar`)
+                .setImage(`${user}`)
+
+            message.channel.send(exampleEmbed);
         }
         // const user = message.mentions.users.displayAvatarURL({ format: "png", dynamic: true });
         const avatarList = message.mentions.users.map(user => {
             return `${user.displayAvatarURL({ format: "png", dynamic: true })}`;
         });
-        
+
         const exampleEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`${taggedUser.username}'s Avatar`)
             .setImage(`${avatarList[0]}`)
-        
+
         message.channel.send(exampleEmbed);
-	},
+    },
 };
