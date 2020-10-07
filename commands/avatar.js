74b8cf9204ea9aa0570259d8828d469f990fd434
alfianaf <alfianaf123@gmail.com>
2020-10-07 18:26:48 +0700

    a

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'avatar',
+    args: false,
     cooldown: 5,
     execute(message, args) {
 
@@ -12,12 +13,12 @@ module.exports = {
             // return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
             const user = message.author.displayAvatarURL({ format: "png", dynamic: true });
 
-            const exampleEmbed = new Discord.MessageEmbed()
+            const aaa = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`${message.author.username}'s Avatar`)
                 .setImage(`${user}`)
 
-            message.channel.send(exampleEmbed);
+            message.channel.send(aaa);
         }
         // const user = message.mentions.users.displayAvatarURL({ format: "png", dynamic: true });
         const avatarList = message.mentions.users.map(user => {
