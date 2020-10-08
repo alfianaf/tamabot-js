e37657a8839055c1b5545f591916b5e19d9491f7
alfianaf <alfianaf123@gmail.com>
2020-10-08 15:24:17 +0700

    a

--- commands/checkroles.js
+++ commands/checkroles.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'checkroles',
     description: 'Check user roles',
     execute(message) {
-        if (message.guild.me.permissions.some(role => role.name === 'Tama')) {
+        if (message.member.roles.cache.some(role => role.name === 'Tama')) {
             return message.channel.send(`${member} has ${role.name}`);
         }
-
+        message.channel.send('You don\'t have a role called Mod.');
         // if (member.hasPermission('KICK_MEMBERS', { checkAdmin: false, checkOwner: false })) {
         //     console.log('This member can kick without allowing admin to override');
         // }
