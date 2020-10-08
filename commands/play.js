1a3dc9e2d347a7e249dd0217eae5432b2b97edf8
alfianaf <alfianaf123@gmail.com>
2020-10-08 18:24:42 +0700

    aaaa

---
+++ commands/play.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'play',
+    description: 'Check roles',
+    cooldown: 8,
+    execute(dispatcher) {
+
+        // Create a dispatcher
+        const dispatcher = connection.play('https://cdn.discordapp.com/attachments/762528512225574913/763723274164895744/SweetSummerRainbowNostalgic_Treasure.mp3');
+
+        dispatcher.on('start', () => {
+            console.log('audio.mp3 is now playing!');
+        });
+
+        dispatcher.on('finish', () => {
+            console.log('audio.mp3 has finished playing!');
+        });
+
+        // Always remember to handle errors appropriately!
+        dispatcher.on('error', console.error);
+    },
+};
