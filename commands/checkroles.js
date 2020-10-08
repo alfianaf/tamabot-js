f8b686d1d8e91a4ae35d83216e010111ba2136d9
alfianaf <alfianaf123@gmail.com>
2020-10-08 15:52:15 +0700

    a

--- commands/checkroles.js
+++ commands/checkroles.js
@@ -3,12 +3,8 @@ module.exports = {
     description: 'Check roles',
     cooldown: 8,
     execute(message) {
-        if (message.member.roles.cache.some(role => role.name === 'Tama')) {
-            try{ message.channel.send(`${member} has ${role.name}`);}
-            catch (error) {
-                console.error(error);
-                message.reply('a');
-            }
+        if (message.member.roles.cache.some(role => role.name === 'e')) {
+            message.channel.send(`${message.member} has ${role.name}`);
         }
         message.channel.send(`e`);
 
